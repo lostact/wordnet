579ef330d4e99025092707c5a12c675a2e0e5251
mohammad <lostact@gmail.com>
2019-02-06 16:59:27 +0330

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wordnik-pronounce',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/lostact/wordnik-pronounce',
+      author='lostact',
+      author_email='lostact@gmail',
+      license='MIT',
+      packages=['wordnik_pronounce'],
+      zip_safe=False)
